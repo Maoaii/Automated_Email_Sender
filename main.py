ed3cb49cc7965d2fb929d03e8403620676756b95
Maoaii <65407819+Maoaii@users.noreply.github.com>
2022-07-30 14:04:03 +0100

    Got environment variables working

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from ui import EmailSenderUI
 from emailsenderbrain import EmailSender
 
 # Set up email to send from
-try:
-    with open("data.json") as data_file:
-        data = json.load(data_file)
-        os.environ["EMAIL_SENDER"] = data["EMAIL_SENDER"]
-        os.environ["EMAIL_PASSWORD"] = data["EMAIL_PASSWORD"]
-except FileNotFoundError:
-    print("No such file exists")
-    quit()
-
 email_sender = os.environ.get("EMAIL_SENDER")
 email_password = os.environ.get("EMAIL_PASSWORD")
 
